e432220ff3e666399e2b5bfd3c18f0c64de8321c
eslam <eslammongy995@gmail.com>
2024-09-04 12:33:02 +0300

    inject local quotes bloc into the container

--- quotable/lib/injection_container.dart
+++ quotable/lib/injection_container.dart
@@ -13,6 +13,7 @@ import 'package:quotable/features/authors/data/datasource/authors_api_service.da
 import 'package:quotable/features/authors/domain/repository/authors_repository.dart';
 import 'package:quotable/features/quotes/data/repository/quote_repository_impl.dart';
 import 'package:quotable/features/authors/data/repository/authors_repository_impl.dart';
+import 'package:quotable/features/quotes/presentation/bloc/local/local_quote_bloc.dart';
 import 'package:quotable/features/categories/domain/repository/category_repository.dart';
 import 'package:quotable/features/categories/data/datasource/category_api_services.dart';
 import 'package:quotable/features/authors/domain/usecaces/fetch_all_authors_usecase.dart';
@@ -130,6 +131,12 @@ Future<void> initializeDependencies() async {
       ));
 
   getIt.registerFactory<AuthorsBloc>(() => AuthorsBloc(
+        getIt(),
+        getIt(),
+      ));
+
+  getIt.registerFactory<LocalQuoteBloc>(() => LocalQuoteBloc(
+        getIt(),
         getIt(),
         getIt(),
       ));
